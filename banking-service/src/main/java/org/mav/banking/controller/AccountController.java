97f4e6a45d767f5114c6533e5d47b11853ccfb48
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2024-03-30 20:49:33 -0400

    Add endpoints to controllers

--- banking-service/src/main/java/org/mav/banking/controller/AccountController.java
+++ banking-service/src/main/java/org/mav/banking/controller/AccountController.java
@@ -1,4 +1,36 @@
 package org.mav.banking.controller;
 
+import org.mav.banking.dto.AccountCreateRequest;
+import org.mav.banking.dto.AccountResponse;
+import org.mav.banking.dto.BalanceResponse;
+import org.mav.banking.dto.DepositRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/accounts")
 public class AccountController {
+
+    @PostMapping
+    public ResponseEntity<AccountResponse> createAccount(@RequestBody AccountCreateRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new AccountResponse());
+    }
+
+    @DeleteMapping("/{accountId}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable String accountId) {
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{accountId}/deposit")
+    public ResponseEntity<BalanceResponse> deposit(@PathVariable String accountId, @RequestBody DepositRequest request) {
+        return ResponseEntity.ok(new BalanceResponse());
+    }
+
+    @GetMapping("/{accountId}/balance")
+    public ResponseEntity<BalanceResponse> getBalance(@PathVariable String accountId) {
+        return ResponseEntity.ok(new BalanceResponse());
+    }
 }
--- banking-service/src/main/java/org/mav/banking/controller/TransactionController.java
+++ banking-service/src/main/java/org/mav/banking/controller/TransactionController.java
@@ -1,4 +1,19 @@
 package org.mav.banking.controller;
 
+import org.mav.banking.dto.TransactionResponse;
+import org.mav.banking.dto.TransferRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
 public class TransactionController {
+
+    @PostMapping("/transfer")
+    public ResponseEntity<TransactionResponse> transfer(@RequestBody TransferRequest request) {
+        return ResponseEntity.ok(new TransactionResponse());
+    }
 }
