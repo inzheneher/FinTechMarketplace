0ee9418953984459a33c398c72eba02f9e4c6f18
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2024-03-31 18:45:35 -0400

    Update account controller

--- banking-service/src/main/java/org/mav/banking/controller/AccountController.java
+++ banking-service/src/main/java/org/mav/banking/controller/AccountController.java
@@ -1,36 +1,48 @@
 package org.mav.banking.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.mav.banking.dto.AccountCreateRequest;
 import org.mav.banking.dto.AccountResponse;
 import org.mav.banking.dto.BalanceResponse;
 import org.mav.banking.dto.DepositRequest;
+import org.mav.banking.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+
 @RestController
 @RequestMapping("/accounts")
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final AccountService accountService;
 
     @PostMapping
     public ResponseEntity<AccountResponse> createAccount(@RequestBody AccountCreateRequest request) {
+        AccountResponse accountResponse = accountService.createAccount(request);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(new AccountResponse());
+                .body(accountResponse);
     }
 
     @DeleteMapping("/{accountId}")
-    public ResponseEntity<Void> deleteAccount(@PathVariable String accountId) {
+    public ResponseEntity<Void> deleteAccount(@PathVariable Long accountId) {
+        accountService.deleteAccount(accountId);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/{accountId}/deposit")
-    public ResponseEntity<BalanceResponse> deposit(@PathVariable String accountId, @RequestBody DepositRequest request) {
-        return ResponseEntity.ok(new BalanceResponse());
+    public ResponseEntity<AccountResponse> deposit(@PathVariable Long accountId, @RequestBody DepositRequest depositRequest) {
+        BigDecimal amount = depositRequest.getAmount();
+        AccountResponse accountResponse = accountService.deposit(accountId, amount);
+        return ResponseEntity.ok(accountResponse);
     }
 
     @GetMapping("/{accountId}/balance")
-    public ResponseEntity<BalanceResponse> getBalance(@PathVariable String accountId) {
-        return ResponseEntity.ok(new BalanceResponse());
+    public ResponseEntity<BalanceResponse> getBalance(@PathVariable Long accountId) {
+        BalanceResponse balanceResponse = accountService.getBalance(accountId);
+        return ResponseEntity.ok(balanceResponse);
     }
 }
