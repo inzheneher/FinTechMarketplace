00cc20fbf971568eb5a737b70a150d65d4459f51
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2024-05-14 21:03:05 -0400

    Change a BalanceResponse to a record

--- banking-service/src/main/java/org/mav/banking/dto/BalanceResponse.java
+++ banking-service/src/main/java/org/mav/banking/dto/BalanceResponse.java
@@ -1,14 +1,6 @@
 package org.mav.banking.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class BalanceResponse {
-    private BigDecimal balance;
+public record BalanceResponse(BigDecimal balance) {
 }
