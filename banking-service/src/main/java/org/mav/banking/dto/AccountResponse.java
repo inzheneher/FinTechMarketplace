74ae4d0003a06b2d2895d09cc0b4c8098a8597dc
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2024-05-14 20:57:53 -0400

    Change AccountResponse to a record

--- banking-service/src/main/java/org/mav/banking/dto/AccountResponse.java
+++ banking-service/src/main/java/org/mav/banking/dto/AccountResponse.java
@@ -1,18 +1,6 @@
 package org.mav.banking.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-
 import java.math.BigDecimal;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class AccountResponse {
-    private Long accountId;
-    private String customerName;
-    private String email;
-    private BigDecimal balance;
+public record AccountResponse(Long accountId, String username, String email, BigDecimal balance) {
 }
