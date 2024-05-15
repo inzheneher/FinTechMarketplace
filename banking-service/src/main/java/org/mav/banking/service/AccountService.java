29bdbdab72876740ca6899f9ab77421260967b98
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2024-05-14 20:58:36 -0400

    Update AccountService to use records

--- banking-service/src/main/java/org/mav/banking/service/AccountService.java
+++ banking-service/src/main/java/org/mav/banking/service/AccountService.java
@@ -1,11 +1,13 @@
 package org.mav.banking.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.mav.banking.dto.AccountCreateRequest;
 import org.mav.banking.dto.AccountResponse;
 import org.mav.banking.dto.BalanceResponse;
 import org.mav.banking.entity.Account;
 import org.mav.banking.repository.AccountRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,30 +16,34 @@ import java.math.BigDecimal;
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class AccountService {
 
     private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public AccountResponse createAccount(AccountCreateRequest request) {
-        if (request.getEmail() == null || request.getCustomerName() == null || request.getSsn() == null) {
+        if (request.email() == null || request.username() == null || request.ssn() == null) {
+            log.error("Email, Customer Name, and SSN must be provided");
             throw new IllegalArgumentException("Email, Customer Name, and SSN must be provided");
         }
 
-        boolean existByEmail = accountRepository.existsByEmail(request.getEmail());
-        boolean existBySsn = accountRepository.existsBySsn(request.getSsn());
+        boolean existByEmail = accountRepository.existsByEmail(request.email());
+        boolean existBySsn = accountRepository.existsBySsn(request.ssn());
         if (existByEmail || existBySsn) {
             throw new IllegalArgumentException("Account with given email or SSN already exists");
         }
 
         Account account = new Account();
-        account.setCustomerName(request.getCustomerName());
-        account.setEmail(request.getEmail());
-        account.setSsn(request.getSsn());
+        account.setUsername(request.username());
+        account.setEmail(request.email());
+        account.setPassword(passwordEncoder.encode(request.password()));
+        account.setSsn(request.ssn());
         account.setBalance(BigDecimal.ZERO);
 
         account = accountRepository.save(account);
 
-        return new AccountResponse(account.getId(), account.getCustomerName(), account.getEmail(), account.getBalance());
+        return new AccountResponse(account.getId(), account.getUsername(), account.getEmail(), account.getBalance());
     }
 
     public void deleteAccount(Long accountId) {
@@ -53,7 +59,7 @@ public class AccountService {
                 .orElseThrow(() -> new IllegalArgumentException("Account with given ID does not exist"));
         account.setBalance(account.getBalance().add(amount));
         account = accountRepository.save(account);
-        return new AccountResponse(account.getId(), account.getCustomerName(), account.getEmail(), account.getBalance());
+        return new AccountResponse(account.getId(), account.getUsername(), account.getEmail(), account.getBalance());
     }
 
     @Transactional(readOnly = true)
