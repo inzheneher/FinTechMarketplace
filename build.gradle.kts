0867deb731aad43cb9f1d9132e8b1c6cd02dbb69
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2024-05-14 21:05:28 -0400

    Update a build.gradle.kts for the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("java")
-    id("org.springframework.boot") version "2.7.0" apply false
+    java
+    id("org.springframework.boot") version "3.2.5" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
 }
 
@@ -15,6 +15,11 @@ subprojects {
     dependencies {
         testImplementation("org.projectlombok:lombok:1.18.32")
         testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.4")
+        testImplementation("org.springframework.security:spring-security-test")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+        testImplementation("org.mockito:mockito-core:5.12.0")
+        testImplementation("org.mockito:mockito-junit-jupiter:5.12.0")
         testAnnotationProcessor("org.projectlombok:lombok:1.18.32")
     }
 }
