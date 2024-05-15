e7bb2bb32c0f9b48089cbb83e41a18c1a0f0cd79
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2024-05-14 21:17:22 -0400

    Add role entity class

---
+++ banking-service/src/main/java/org/mav/banking/entity/Role.java
@@ -0,0 +1,28 @@
+package org.mav.banking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "roles", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "name")
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<Account> accounts;
+}
