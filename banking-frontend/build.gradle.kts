1a8f32af811c3f4297485bd7cc58659e6db9cf21
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2024-05-14 21:03:57 -0400

    Add a build.gradle.kts to the frontend module

---
+++ banking-frontend/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("java")
+    id("org.springframework.boot")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
