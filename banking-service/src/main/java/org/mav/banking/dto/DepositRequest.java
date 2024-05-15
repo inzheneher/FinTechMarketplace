9c34171c6b11661b32ccd1b14609f449cdf77940
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2024-05-14 21:07:29 -0400

    Change DepositRequest to record

--- banking-service/src/main/java/org/mav/banking/dto/DepositRequest.java
+++ banking-service/src/main/java/org/mav/banking/dto/DepositRequest.java
@@ -1,12 +1,6 @@
 package org.mav.banking.dto;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 
-@Data
-@NoArgsConstructor
-public class DepositRequest {
-    private BigDecimal amount;
+public record DepositRequest(BigDecimal amount) {
 }
