575ff64326ff59c19683abfd6722066df61a933e
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2024-05-14 20:57:21 -0400

    Add findByEmail method to the AccountRepository

--- banking-service/src/main/java/org/mav/banking/repository/AccountRepository.java
+++ banking-service/src/main/java/org/mav/banking/repository/AccountRepository.java
@@ -7,4 +7,6 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     boolean existsByEmail(String email);
 
     boolean existsBySsn(String ssn);
+
+    Account findByEmail(String email);
 }
